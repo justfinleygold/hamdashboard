55edbd0a11b35f4b9a32a81e2e6a2a9aa01fb42b
Finley Gold <35870010+justfinleygold@users.noreply.github.com>
2024-12-03 08:18:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const aURL = [
   ["add10d", "BACK", "#", "1", "R"],
   ["ff9100", "Refresh", "#", "1"],
   ["0dd1a7", "Help", "#", "1"],
-  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/VA3HDL", "1.7"],
+  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/NC4FG", "1.7"],
   [
     "2196F3",
     "CONTEST",
